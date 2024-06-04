4adebdd73329450e27b1a82a33aedf0bcfc169e3
nickrmurphy <hi@nickmurphy.io>
2024-06-03 21:54:52 -0500

    init ToDos POC

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,17 +1,45 @@
+// import { Button } from "@/components/ui/button";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { client } from "@/triplit/client";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@triplit/react";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
+const query = client.query("todos");
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 function Index() {
+  const { results, fetching } = useQuery(client, query);
+
   return (
     <div className="p-2 space-y-10">
       <h3 className="text-3xl font-bold">Welcome Home!</h3>
-      <Input placeholder="Enter your name" />
-      <Button>Say Hello</Button>
+      {fetching ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {/* TODO: Remove ! */}
+          {Array.from(results!).map(([id, todo]) => (
+            <li key={id}>{todo.title}</li>
+          ))}
+        </ul>
+      )}
+      <Button
+        onClick={async () => {
+          await client.insert("todos", {
+            title: `Random To Do #${getRandomInt(100)}`,
+            description: "This is a random To Do",
+          });
+        }}
+      >
+        Add a random To Do
+      </Button>
     </div>
   );
 }
