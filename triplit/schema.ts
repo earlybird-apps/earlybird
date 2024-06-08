64c679d315727fd5f5ae26b08baaa1da02acf00d
nickrmurphy <hi@nickmurphy.io>
2024-06-08 10:38:41 -0500

    TECH: Seed Data

--- triplit/schema.ts
+++ triplit/schema.ts
@@ -5,13 +5,6 @@ import { Schema as S, ClientSchema } from '@triplit/client';
  * For more information on schemas, see the docs: https://www.triplit.dev/docs/schemas
  */
 export const schema = {
-  todos: {
-    schema: S.Schema({
-      id: S.Id(),
-      title: S.String(),
-      description: S.String(),
-    }),
-  },
   users: {
     schema: S.Schema({
       id: S.Id(),
@@ -27,13 +20,13 @@ export const schema = {
     }),
     rules: {
       read: {
-        'author-is-user': {
+        'owner-is-user': {
           description: 'Users can only view budgets they own',
           filter: [['user_id', '=', '$session.SESSION_USER_ID']],
         },
       },
       write: {
-        'author-is-user': {
+        'owner-is-user': {
           description: 'Users can only edit their own budgets',
           filter: [['user_id', '=', '$session.SESSION_USER_ID']],
         },
@@ -50,15 +43,83 @@ export const schema = {
     }),
     rules: {
       read: {
-        'author-is-user': {
+        'owner-is-user': {
           description: 'Users can only view accounts they own',
           filter: [['budget.user_id', '=', '$session.SESSION_USER_ID']],
         },
       },
       write: {
-        'author-is-user': {
+        'owner-is-user': {
           description: 'Users can only edit their own accounts',
           filter: [['budget.user_id', '=', '$session.SESSION_USER_ID']],
+        },
+      },
+    },
+  },
+  transactions: {
+    schema: S.Schema({
+      id: S.Id(),
+      amount: S.Number(),
+      account_id: S.String(),
+      account: S.RelationById("accounts", "$account_id"),
+    }),
+    rules: {
+      read: {
+        'owner-is-user': {
+          description: 'Users can only view transactions for accounts they own',
+          filter: [['account.budget.user_id', '=', '$session.SESSION_USER_ID']],
+        },
+      },
+      write: {
+        'owner-is-user': {
+          description: 'Users can only edit transactions for accounts they own',
+          filter: [['account.budget.user_id', '=', '$session.SESSION_USER_ID']],
+        },
+      },
+    },
+  },
+  categories: {
+    schema: S.Schema({
+      id: S.Id(),
+      name: S.String(),
+      budget_id: S.String(),
+      budget: S.RelationById("budgets", "$budget_id"),
+    }),
+    rules: {
+      read: {
+        'owner-is-user': {
+          description: 'Users can only view categories for budgets they own',
+          filter: [['budget.user_id', '=', '$session.SESSION_USER_ID']],
+        },
+      },
+      write: {
+        'owner-is-user': {
+          description: 'Users can only edit categories for budgets they own',
+          filter: [['budget.user_id', '=', '$session.SESSION_USER_ID']],
+        },
+      },
+    },
+  },
+  assignments: {
+    schema: S.Schema({
+      id: S.Id(),
+      amount: S.Number({ default: 0 }),
+      month: S.Number(),
+      year: S.Number(),
+      category_id: S.String(),
+      category: S.RelationById("categories", "$category_id"),
+    }),
+    rules: {
+      read: {
+        'owner-is-user': {
+          description: 'Users can only view assignments for categories they own',
+          filter: [['category.budget.user_id', '=', '$session.SESSION_USER_ID']],
+        },
+      },
+      write: {
+        'owner-is-user': {
+          description: 'Users can only edit assignments for categories they own',
+          filter: [['category.budget.user_id', '=', '$session.SESSION_USER_ID']],
         },
       },
     },
---
+++ triplit/seeds/seed-test-data.ts
@@ -0,0 +1,83 @@
+import { BulkInsert } from "@triplit/client";
+import { schema } from "../schema.js";
+
+const user = {
+  id: "n9Q5Hkks3OCuLDQAtI0IV",
+  name: "Nick",
+};
+
+export default function seed(): BulkInsert<typeof schema> {
+  return {
+    users: [user],
+    budgets: [{
+      id: "1",
+      name: "Slow Growth",
+      user_id: user.id,
+    }, {
+      id: "2",
+      name: "Empty",
+      user_id: user.id,
+    }],
+    accounts: [{
+      id: "1",
+      name: "Checking",
+      balance: 1000,
+      budget_id: "1",
+    },
+    {
+      id: "2",
+      name: "Savings",
+      balance: 5000,
+      budget_id: "1",
+    }],
+    transactions: [{
+      amount: 1200,
+      account_id: "1",
+    },
+    {
+      amount: -200,
+      account_id: "1",
+    },
+    {
+      amount: 4000,
+      account_id: "2",
+    },
+    {
+      amount: 1000,
+      account_id: "2",
+    }],
+    categories: [{
+      id: "1",
+      name: "Groceries",
+      budget_id: "1",
+    },
+    {
+      id: "2",
+      name: "Utilities",
+      budget_id: "1",
+    },
+    {
+      id: "3",
+      name: "Savings",
+      budget_id: "1",
+    }],
+    assignments: [{
+      category_id: "1",
+      amount: 80,
+      month: 5,
+      year: 2024,
+    },
+    {
+      category_id: "2",
+      amount: 100,
+      month: 5,
+      year: 2024,
+    },
+    {
+      category_id: "3",
+      amount: 500,
+      month: 5,
+      year: 2024,
+    }],
+  };
+}
