d1ceeef3c60a17410c85ef517b4fc13706604c00
nickrmurphy <hi@nickmurphy.io>
2024-06-10 16:30:54 -0500

    TECH: computeCurrentSnapshot + tests

--- triplit/types.ts
+++ triplit/types.ts
@@ -3,3 +3,5 @@ import { Entity } from "@triplit/client";
 
 export type Budget = Entity<typeof schema, "budgets">;
 export type Category = Entity<typeof schema, "categories">;
+export type Transaction = Entity<typeof schema, "transactions">;
+export type Assignment = Entity<typeof schema, "assignments">;
